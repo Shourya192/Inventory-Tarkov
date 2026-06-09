0db638c4f82a1e2a8968fd1d7311516232b4c5bf
Shourya192 <90411872+Shourya192@users.noreply.github.com>
2026-06-09 13:09:18 +0530

    Update EquipmentPanelRenderer.java

--- src/main/java/com/tarkovinventory/client/screen/modules/EquipmentPanelRenderer.java
+++ src/main/java/com/tarkovinventory/client/screen/modules/EquipmentPanelRenderer.java
@@ -2,7 +2,7 @@ package com.tarkovinventory.client.screen.modules;
 
 import net.minecraft.client.gui.GuiGraphics;
 
-public class EquipmentPanelRenderer implements IModule {
+public class EquipmentPanelRenderer {
 
     private int left;
     private int top;
@@ -12,11 +12,10 @@ public class EquipmentPanelRenderer implements IModule {
         this.top = top;
     }
 
-    @Override
-    public void render(GuiGraphics g, int left, int top, int mouseX, int mouseY) {
+    public void render(GuiGraphics g) {
 
-        int x = this.left + 7;
-        int y = this.top + 7;
+        int x = left + 7;
+        int y = top + 7;
 
         g.fill(x, y, x + 26, y + 26, 0xFF1A1A1A);
         g.fill(x + 40, y, x + 70, y + 30, 0xFF1A1A1A);
@@ -29,4 +28,8 @@ public class EquipmentPanelRenderer implements IModule {
         g.fill(x, y + 110, x + 40, y + 150, 0xFF1A1A1A);
         g.fill(x + 80, y + 110, x + 120, y + 150, 0xFF1A1A1A);
     }
+
+    // input stubs (so screen doesn't break later)
+    public void mouseClicked(double mx, double my, int btn, int x, int y) {}
+    public void mouseReleased(double mx, double my, int btn) {}
 }
