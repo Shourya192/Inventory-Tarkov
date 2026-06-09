fc6d9d35401bebfd9f9e57cf3475216514889e4d
Shourya192 <90411872+Shourya192@users.noreply.github.com>
2026-06-09 13:24:05 +0530

    Update EquipmentPanelRenderer.java

--- src/main/java/com/tarkovinventory/client/screen/modules/EquipmentPanelRenderer.java
+++ src/main/java/com/tarkovinventory/client/screen/modules/EquipmentPanelRenderer.java
@@ -2,6 +2,9 @@ package com.tarkovinventory.client.screen.modules;
 
 import net.minecraft.client.gui.GuiGraphics;
 
+/**
+ * Equipment panel (clean Tarkov-style blocks).
+ */
 public class EquipmentPanelRenderer {
 
     private int left;
@@ -14,22 +17,22 @@ public class EquipmentPanelRenderer {
 
     public void render(GuiGraphics g) {
 
-        int x = left + 7;
-        int y = top + 7;
+        int x = left;
+        int y = top;
 
-        g.fill(x, y, x + 26, y + 26, 0xFF1A1A1A);
-        g.fill(x + 40, y, x + 70, y + 30, 0xFF1A1A1A);
-        g.fill(x + 90, y, x + 116, y + 26, 0xFF1A1A1A);
-        g.fill(x + 130, y, x + 156, y + 26, 0xFF1A1A1A);
+        // PANEL BACKGROUND
+        g.fill(x, y, x + 180, y + 180, 0xFF141414);
 
-        g.fill(x, y + 50, x + 40, y + 90, 0xFF1A1A1A);
-        g.fill(x + 60, y + 50, x + 100, y + 90, 0xFF1A1A1A);
+        // HEADER
+        g.fill(x, y, x + 180, y + 18, 0xFF1C1C1C);
 
-        g.fill(x, y + 110, x + 40, y + 150, 0xFF1A1A1A);
-        g.fill(x + 80, y + 110, x + 120, y + 150, 0xFF1A1A1A);
+        // SLOT BLOCKS
+        g.fill(x + 10, y + 25, x + 50, y + 55, 0xFF222222);  // head
+        g.fill(x + 60, y + 25, x + 160, y + 55, 0xFF222222); // chest
+
+        g.fill(x + 10, y + 65, x + 50, y + 95, 0xFF222222);  // legs
+        g.fill(x + 60, y + 65, x + 160, y + 95, 0xFF222222); // rig
+
+        g.fill(x + 10, y + 105, x + 160, y + 140, 0xFF222222);
     }
-
-    // input stubs (so screen doesn't break later)
-    public void mouseClicked(double mx, double my, int btn, int x, int y) {}
-    public void mouseReleased(double mx, double my, int btn) {}
 }
