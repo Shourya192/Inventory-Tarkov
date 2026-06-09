05d0316cc295bf384a780b839d68427eea928191
Shourya192 <90411872+Shourya192@users.noreply.github.com>
2026-06-09 14:55:31 +0530

    Update ClientSetup.java

--- src/main/java/com/tarkovinventory/client/ClientSetup.java
+++ src/main/java/com/tarkovinventory/client/ClientSetup.java
@@ -1,24 +1,22 @@
 package com.tarkovinventory.client;
 
 import com.tarkovinventory.TarkovInventoryMod;
-import com.tarkovinventory.client.screen.TarkovInventoryScreen;
-import com.tarkovinventory.registry.ModMenuTypes;
-import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
-@Mod.EventBusSubscriber(modid = TarkovInventoryMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+/**
+ * CLEAN CLIENT SETUP (STANDALONE UI VERSION)
+ * - NO MenuScreens.register (we are not using containers anymore)
+ * - ONLY keybind registration
+ */
+@Mod.EventBusSubscriber(
+        modid = TarkovInventoryMod.MOD_ID,
+        bus = Mod.EventBusSubscriber.Bus.MOD,
+        value = Dist.CLIENT
+)
 public class ClientSetup {
-
-    @SubscribeEvent
-    public static void onClientSetup(FMLClientSetupEvent event) {
-        event.enqueueWork(() ->
-                MenuScreens.register(ModMenuTypes.TARKOV_INVENTORY.get(), TarkovInventoryScreen::new)
-        );
-    }
 
     @SubscribeEvent
     public static void onRegisterKeyMappings(RegisterKeyMappingsEvent event) {
