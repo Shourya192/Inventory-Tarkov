f3f3456cce612bc71dc098328649369b55cac37f
Shourya192 <90411872+Shourya192@users.noreply.github.com>
2026-06-09 14:59:22 +0530

    Update VicinityRenderer.java

--- src/main/java/com/tarkovinventory/client/screen/modules/VicinityRenderer.java
+++ src/main/java/com/tarkovinventory/client/screen/modules/VicinityRenderer.java
@@ -1,19 +1,18 @@
 package com.tarkovinventory.client.screen.modules;
 
-import com.tarkovinventory.client.screen.layout.Panel;
 import net.minecraft.client.gui.GuiGraphics;
 
 public class VicinityRenderer {
 
-    public void render(GuiGraphics g, Panel p) {
+    public void render(GuiGraphics g, int left, int top) {
 
-        p.draw(g);
+        int x = left + 450;
+        int y = top + 10;
+
+        g.fill(x, y, x + 140, y + 200, 0xFF101010);
 
         for (int i = 0; i < 10; i++) {
-
-            int y = p.y + 22 + i * 18;
-
-            g.fill(p.x + 6, y, p.x + p.w - 6, y + 14, 0xFF1A1A1A);
+            g.fill(x + 5, y + 5 + i * 18, x + 135, y + 20 + i * 18, 0xFF1A1A1A);
         }
     }
 }
