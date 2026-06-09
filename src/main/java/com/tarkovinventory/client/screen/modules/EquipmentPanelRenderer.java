7fad7e1b1e249395bebed746404653eba497d2ae
Shourya192 <90411872+Shourya192@users.noreply.github.com>
2026-06-09 14:38:40 +0530

    Update EquipmentPanelRenderer.java

--- src/main/java/com/tarkovinventory/client/screen/modules/EquipmentPanelRenderer.java
+++ src/main/java/com/tarkovinventory/client/screen/modules/EquipmentPanelRenderer.java
@@ -12,12 +12,12 @@ public class EquipmentPanelRenderer {
         int x = p.x + 10;
         int y = p.y + 25;
 
-        g.fill(x, y, x + 26, y + 26, 0xFF222222);
-        g.fill(x + 40, y, x + 70, y + 30, 0xFF222222);
-        g.fill(x + 90, y, x + 116, y + 26, 0xFF222222);
-        g.fill(x + 130, y, x + 156, y + 26, 0xFF222222);
+        g.fill(x, y, x + 26, y + 26, 0xFF2A2A2A);
+        g.fill(x + 40, y, x + 70, y + 30, 0xFF2A2A2A);
+        g.fill(x + 90, y, x + 116, y + 26, 0xFF2A2A2A);
+        g.fill(x + 130, y, x + 156, y + 26, 0xFF2A2A2A);
 
-        g.fill(x, y + 50, x + 40, y + 90, 0xFF222222);
-        g.fill(x + 60, y + 50, x + 100, y + 90, 0xFF222222);
+        g.fill(x, y + 50, x + 40, y + 90, 0xFF2A2A2A);
+        g.fill(x + 60, y + 50, x + 100, y + 90, 0xFF2A2A2A);
     }
 }
