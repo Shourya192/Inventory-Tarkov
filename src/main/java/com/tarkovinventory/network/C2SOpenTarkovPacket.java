657fae317b7156373aae7d1612258afa1bacd0a7
Shourya192 <90411872+Shourya192@users.noreply.github.com>
2026-06-09 14:47:51 +0530

    Update C2SOpenTarkovPacket.java

--- src/main/java/com/tarkovinventory/network/C2SOpenTarkovPacket.java
+++ src/main/java/com/tarkovinventory/network/C2SOpenTarkovPacket.java
@@ -9,20 +9,19 @@ import java.util.function.Supplier;
 
 public class C2SOpenTarkovPacket {
 
-    public C2SOpenTarkovPacket() {}
+    public static void encode(C2SOpenTarkovPacket msg, FriendlyByteBuf buf) {}
 
-    public C2SOpenTarkovPacket(FriendlyByteBuf buf) {}
+    public static C2SOpenTarkovPacket decode(FriendlyByteBuf buf) {
+        return new C2SOpenTarkovPacket();
+    }
 
-    public void toBytes(FriendlyByteBuf buf) {}
-
-    public boolean handle(Supplier<NetworkEvent.Context> ctx) {
+    public static void handle(C2SOpenTarkovPacket msg, Supplier<NetworkEvent.Context> ctx) {
 
         ctx.get().enqueueWork(() -> {
 
             ServerPlayer player = ctx.get().getSender();
             if (player == null) return;
 
-            // send response to client
             ModNetwork.CHANNEL.send(
                     PacketDistributor.PLAYER.with(() -> player),
                     new S2COpenTarkovPacket()
@@ -30,6 +29,5 @@ public class C2SOpenTarkovPacket {
         });
 
         ctx.get().setPacketHandled(true);
-        return true;
     }
 }
