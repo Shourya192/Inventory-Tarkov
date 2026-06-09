eb78546bf3e28461c3c67929fb76eee1c07d532d
Shourya192 <90411872+Shourya192@users.noreply.github.com>
2026-06-09 13:10:24 +0530

    Update VicinityRenderer.java

--- src/main/java/com/tarkovinventory/client/screen/modules/VicinityRenderer.java
+++ src/main/java/com/tarkovinventory/client/screen/modules/VicinityRenderer.java
@@ -2,32 +2,21 @@ package com.tarkovinventory.client.screen.modules;
 
 import net.minecraft.client.gui.GuiGraphics;
 
-public class VicinityRenderer implements IModule {
+public class VicinityRenderer {
 
-    private int left;
-    private int top;
+    public void render(GuiGraphics g, int left, int top) {
 
-    public void init(int left, int top) {
-        this.left = left;
-        this.top = top;
-    }
-
-    @Override
-    public void render(GuiGraphics g, int left, int top, int mouseX, int mouseY) {
-
-        int x = this.left + 450;
-        int y = this.top + 10;
+        int x = left + 450;
+        int y = top + 10;
 
         g.fill(x, y, x + 140, y + 200, 0xFF101010);
 
         for (int i = 0; i < 10; i++) {
-            g.fill(
-                    x + 5,
-                    y + 5 + i * 18,
-                    x + 135,
-                    y + 20 + i * 18,
-                    0xFF1A1A1A
-            );
+            g.fill(x + 5, y + 5 + i * 18, x + 135, y + 20 + i * 18, 0xFF1A1A1A);
         }
     }
+
+    public void mouseClicked(double mx, double my, int btn, int x, int y) {}
+    public void mouseReleased(double mx, double my, int btn) {}
+    public void mouseDragged(double mx, double my, int btn, Object dragState) {}
 }
