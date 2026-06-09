d884de5859fd391aa70504e46bbbe3a6cd5d4266
Shourya192 <90411872+Shourya192@users.noreply.github.com>
2026-06-09 14:45:19 +0530

    Update ModNetwork.java

--- src/main/java/com/tarkovinventory/network/ModNetwork.java
+++ src/main/java/com/tarkovinventory/network/ModNetwork.java
@@ -19,40 +19,69 @@ public final class ModNetwork {
     private static int id = 0;
 
     public static void register() {
-        CHANNEL.registerMessage(id++,
+
+        CHANNEL.registerMessage(
+                id++,
                 C2SOpenTarkovPacket.class,
                 C2SOpenTarkovPacket::encode,
                 C2SOpenTarkovPacket::decode,
-                C2SOpenTarkovPacket::handle);
-        CHANNEL.registerMessage(id++,
+                C2SOpenTarkovPacket::handle
+        );
+
+        CHANNEL.registerMessage(
+                id++,
+                S2COpenTarkovPacket.class,
+                S2COpenTarkovPacket::encode,
+                S2COpenTarkovPacket::decode,
+                S2COpenTarkovPacket::handle
+        );
+
+        CHANNEL.registerMessage(
+                id++,
                 C2SPickupItemPacket.class,
                 C2SPickupItemPacket::encode,
                 C2SPickupItemPacket::decode,
-                C2SPickupItemPacket::handle);
-        CHANNEL.registerMessage(id++,
+                C2SPickupItemPacket::handle
+        );
+
+        CHANNEL.registerMessage(
+                id++,
                 C2SLootAllPacket.class,
                 C2SLootAllPacket::encode,
                 C2SLootAllPacket::decode,
-                C2SLootAllPacket::handle);
-        CHANNEL.registerMessage(id++,
+                C2SLootAllPacket::handle
+        );
+
+        CHANNEL.registerMessage(
+                id++,
                 C2STakeFromCorpsePacket.class,
                 C2STakeFromCorpsePacket::encode,
                 C2STakeFromCorpsePacket::decode,
-                C2STakeFromCorpsePacket::handle);
-        CHANNEL.registerMessage(id++,
+                C2STakeFromCorpsePacket::handle
+        );
+
+        CHANNEL.registerMessage(
+                id++,
                 S2CCorpseContentsPacket.class,
                 S2CCorpseContentsPacket::encode,
                 S2CCorpseContentsPacket::decode,
-                S2CCorpseContentsPacket::handle);
-        CHANNEL.registerMessage(id++,
+                S2CCorpseContentsPacket::handle
+        );
+
+        CHANNEL.registerMessage(
+                id++,
                 C2SRigSlotPacket.class,
                 C2SRigSlotPacket::encode,
                 C2SRigSlotPacket::decode,
-                C2SRigSlotPacket::handle);
-        CHANNEL.registerMessage(id++,
+                C2SRigSlotPacket::handle
+        );
+
+        CHANNEL.registerMessage(
+                id++,
                 C2SRigPlacePacket.class,
                 C2SRigPlacePacket::encode,
                 C2SRigPlacePacket::decode,
-                C2SRigPlacePacket::handle);
+                C2SRigPlacePacket::handle
+        );
     }
 }
