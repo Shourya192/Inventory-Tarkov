927997caafbe7a4610b679b51d72c359d4b7ed85
Shourya192 <90411872+Shourya192@users.noreply.github.com>
2026-06-09 14:25:59 +0530

    Update UIRoot.java

--- src/main/java/com/tarkovinventory/client/screen/layout/UIRoot.java
+++ src/main/java/com/tarkovinventory/client/screen/layout/UIRoot.java
@@ -1,15 +1,12 @@
 package com.tarkovinventory.client.screen.layout;
 
-/**
- * Single anchor for ALL UI rendering.
- */
 public class UIRoot {
 
     public final int x;
     public final int y;
 
-    public UIRoot(int screenWidth, int screenHeight) {
-        this.x = (screenWidth - 600) / 2;
-        this.y = (screenHeight - 300) / 2;
+    public UIRoot(int width, int height) {
+        this.x = (width - 650) / 2;
+        this.y = (height - 350) / 2;
     }
 }
