97cec3226caded881ee207ee76f0c3effa99318d
Shourya192 <90411872+Shourya192@users.noreply.github.com>
2026-06-09 13:09:59 +0530

    Update LootPanelRenderer.java

--- src/main/java/com/tarkovinventory/client/screen/modules/LootPanelRenderer.java
+++ src/main/java/com/tarkovinventory/client/screen/modules/LootPanelRenderer.java
@@ -2,32 +2,21 @@ package com.tarkovinventory.client.screen.modules;
 
 import net.minecraft.client.gui.GuiGraphics;
 
-public class LootPanelRenderer implements IModule {
+public class LootPanelRenderer {
 
-    private int left;
-    private int top;
+    public void render(GuiGraphics g, int left, int top) {
 
-    public void init(int left, int top) {
-        this.left = left;
-        this.top = top;
-    }
-
-    @Override
-    public void render(GuiGraphics g, int left, int top, int mouseX, int mouseY) {
-
-        int x = this.left + 300;
-        int y = this.top + 10;
+        int x = left + 300;
+        int y = top + 10;
 
         g.fill(x, y, x + 150, y + 220, 0xFF2A1010);
 
         for (int i = 0; i < 12; i++) {
-            g.fill(
-                    x + 5,
-                    y + 5 + i * 16,
-                    x + 145,
-                    y + 18 + i * 16,
-                    0xFF3A1A1A
-            );
+            g.fill(x + 5, y + 5 + i * 16, x + 145, y + 18 + i * 16, 0xFF3A1A1A);
         }
     }
+
+    public void mouseClicked(double mx, double my, int btn, int x, int y) {}
+    public void mouseReleased(double mx, double my, int btn) {}
+    public void mouseDragged(double mx, double my, int btn, double dx, double dy, Object dragState) {}
 }
