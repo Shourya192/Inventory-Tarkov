64eed6e74cd7868b2f8cbd03ff1911b7e305e898
Shourya192 <90411872+Shourya192@users.noreply.github.com>
2026-06-09 14:36:55 +0530

    Update UIRoot.java

--- src/main/java/com/tarkovinventory/client/screen/layout/UIRoot.java
+++ src/main/java/com/tarkovinventory/client/screen/layout/UIRoot.java
@@ -5,13 +5,7 @@ public class UIRoot {
     public final int x;
     public final int y;
 
-    public final int width;
-    public final int height;
-
     public UIRoot(int screenWidth, int screenHeight) {
-        this.width = screenWidth;
-        this.height = screenHeight;
-
         this.x = (screenWidth - 650) / 2;
         this.y = (screenHeight - 350) / 2;
     }
