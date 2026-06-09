bf9150f248d756990ddff84777bd996c55214b83
Shourya192 <90411872+Shourya192@users.noreply.github.com>
2026-06-09 14:37:38 +0530

    Update Panel.java

--- src/main/java/com/tarkovinventory/client/screen/layout/Panel.java
+++ src/main/java/com/tarkovinventory/client/screen/layout/Panel.java
@@ -14,17 +14,7 @@ public class Panel {
     }
 
     public void draw(GuiGraphics g) {
-
-        // main panel background
-        g.fill(x, y, x + w, y + h, 0xFF151515);
-
-        // header bar
+        g.fill(x, y, x + w, y + h, 0xFF141414);
         g.fill(x, y, x + w, y + 18, 0xFF1C1C1C);
-
-        // border
-        g.fill(x, y, x + w, y + 1, 0xFF2A2A2A);
-        g.fill(x, y + h - 1, x + w, y + h, 0xFF2A2A2A);
-        g.fill(x, y, x + 1, y + h, 0xFF2A2A2A);
-        g.fill(x + w - 1, y, x + w, y + h, 0xFF2A2A2A);
     }
 }
