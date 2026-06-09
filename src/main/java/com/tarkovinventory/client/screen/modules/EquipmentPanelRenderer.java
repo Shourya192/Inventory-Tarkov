e6b818b4442fa38c46c27280329206f2f4ca615a
Shourya192 <90411872+Shourya192@users.noreply.github.com>
2026-06-09 14:58:37 +0530

    Update EquipmentPanelRenderer.java

--- src/main/java/com/tarkovinventory/client/screen/modules/EquipmentPanelRenderer.java
+++ src/main/java/com/tarkovinventory/client/screen/modules/EquipmentPanelRenderer.java
@@ -1,23 +1,31 @@
 package com.tarkovinventory.client.screen.modules;
 
-import com.tarkovinventory.client.screen.layout.Panel;
 import net.minecraft.client.gui.GuiGraphics;
 
+/**
+ * Equipment UI (fixed standalone version)
+ */
 public class EquipmentPanelRenderer {
 
-    public void render(GuiGraphics g, Panel p) {
+    private int left;
+    private int top;
 
-        p.draw(g);
+    public void init(int left, int top) {
+        this.left = left;
+        this.top = top;
+    }
 
-        int x = p.x + 10;
-        int y = p.y + 25;
+    public void render(GuiGraphics g) {
 
-        g.fill(x, y, x + 26, y + 26, 0xFF2A2A2A);
-        g.fill(x + 40, y, x + 70, y + 30, 0xFF2A2A2A);
-        g.fill(x + 90, y, x + 116, y + 26, 0xFF2A2A2A);
-        g.fill(x + 130, y, x + 156, y + 26, 0xFF2A2A2A);
+        int x = left + 7;
+        int y = top + 7;
 
-        g.fill(x, y + 50, x + 40, y + 90, 0xFF2A2A2A);
-        g.fill(x + 60, y + 50, x + 100, y + 90, 0xFF2A2A2A);
+        g.fill(x, y, x + 26, y + 26, 0xFF1A1A1A); // head
+        g.fill(x + 40, y, x + 70, y + 30, 0xFF1A1A1A); // chest
+        g.fill(x + 90, y, x + 116, y + 26, 0xFF1A1A1A); // legs
+        g.fill(x + 130, y, x + 156, y + 26, 0xFF1A1A1A); // feet
+        g.fill(x, y + 50, x + 40, y + 90, 0xFF1A1A1A); // rig
+        g.fill(x + 60, y + 50, x + 100, y + 90, 0xFF1A1A1A); // backpack
+        g.fill(x, y + 110, x + 40, y + 150, 0xFF1A1A1A); // weapon
     }
 }
