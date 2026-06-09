01f1dc49c078d766cd58937140bbdf881b8d020b
Shourya192 <90411872+Shourya192@users.noreply.github.com>
2026-06-09 13:24:57 +0530

    Update LootPanelRenderer.java

--- src/main/java/com/tarkovinventory/client/screen/modules/LootPanelRenderer.java
+++ src/main/java/com/tarkovinventory/client/screen/modules/LootPanelRenderer.java
@@ -6,17 +6,19 @@ public class LootPanelRenderer {
 
     public void render(GuiGraphics g, int left, int top) {
 
-        int x = left + 300;
-        int y = top + 10;
+        int x = left;
+        int y = top;
 
-        g.fill(x, y, x + 150, y + 220, 0xFF2A1010);
+        g.fill(x, y, x + 160, y + 240, 0xFF141414);
+
+        g.fill(x, y, x + 160, y + 18, 0xFF1C1C1C);
 
         for (int i = 0; i < 12; i++) {
-            g.fill(x + 5, y + 5 + i * 16, x + 145, y + 18 + i * 16, 0xFF3A1A1A);
+            int rowY = y + 22 + i * 16;
+
+            g.fill(x + 5, rowY,
+                   x + 155, rowY + 14,
+                   0xFF222222);
         }
     }
-
-    public void mouseClicked(double mx, double my, int btn, int x, int y) {}
-    public void mouseReleased(double mx, double my, int btn) {}
-    public void mouseDragged(double mx, double my, int btn, double dx, double dy, Object dragState) {}
 }
