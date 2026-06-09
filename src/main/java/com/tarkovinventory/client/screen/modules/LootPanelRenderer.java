7d3d8826de874223f7145af4772178f54a0d7622
Shourya192 <90411872+Shourya192@users.noreply.github.com>
2026-06-09 14:58:59 +0530

    Update LootPanelRenderer.java

--- src/main/java/com/tarkovinventory/client/screen/modules/LootPanelRenderer.java
+++ src/main/java/com/tarkovinventory/client/screen/modules/LootPanelRenderer.java
@@ -1,19 +1,24 @@
 package com.tarkovinventory.client.screen.modules;
 
-import com.tarkovinventory.client.screen.layout.Panel;
 import net.minecraft.client.gui.GuiGraphics;
 
 public class LootPanelRenderer {
 
-    public void render(GuiGraphics g, Panel p) {
+    private int left;
+    private int top;
 
-        p.draw(g);
+    public void render(GuiGraphics g, int left, int top) {
+
+        this.left = left;
+        this.top = top;
+
+        int x = left + 300;
+        int y = top + 10;
+
+        g.fill(x, y, x + 150, y + 220, 0xFF2A1010);
 
         for (int i = 0; i < 12; i++) {
-
-            int y = p.y + 22 + i * 16;
-
-            g.fill(p.x + 6, y, p.x + p.w - 6, y + 12, 0xFF222222);
+            g.fill(x + 5, y + 5 + i * 16, x + 145, y + 18 + i * 16, 0xFF3A1A1A);
         }
     }
 }
