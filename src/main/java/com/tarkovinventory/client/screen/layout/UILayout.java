d0235512dc7714e7c15160cd76c6cefa48da8bb3
Shourya192 <90411872+Shourya192@users.noreply.github.com>
2026-06-09 14:04:22 +0530

    Update UILayout.java

--- src/main/java/com/tarkovinventory/client/screen/layout/UILayout.java
+++ src/main/java/com/tarkovinventory/client/screen/layout/UILayout.java
@@ -1,32 +1,26 @@
 package com.tarkovinventory.client.screen.layout;
 
-/**
- * Returns fixed UI panels.
- * NO logic, NO rendering, ONLY layout.
- */
 public class UILayout {
 
-    private final int left;
-    private final int top;
+    private final UIRoot root;
 
-    public UILayout(int left, int top, int width, int height) {
-        this.left = left;
-        this.top = top;
+    public UILayout(UIRoot root) {
+        this.root = root;
     }
 
     public Panel equipment() {
-        return new Panel(left + 10, top + 10, 180, 180);
+        return new Panel(root.x, root.y, 180, 180);
     }
 
     public Panel grid() {
-        return new Panel(left + 200, top + 40, 220, 220);
+        return new Panel(root.x + 190, root.y + 30, 220, 220);
     }
 
     public Panel loot() {
-        return new Panel(left + 430, top + 10, 160, 240);
+        return new Panel(root.x + 420, root.y, 160, 240);
     }
 
     public Panel vicinity() {
-        return new Panel(left + 430, top + 260, 160, 180);
+        return new Panel(root.x + 420, root.y + 250, 160, 180);
     }
 }
