670fd2413f341cb8a101a73554f38d05975018a6
Shourya192 <90411872+Shourya192@users.noreply.github.com>
2026-06-09 14:03:56 +0530

    Update Panel.java

--- src/main/java/com/tarkovinventory/client/screen/layout/Panel.java
+++ src/main/java/com/tarkovinventory/client/screen/layout/Panel.java
@@ -3,14 +3,11 @@ package com.tarkovinventory.client.screen.layout;
 import net.minecraft.client.gui.GuiGraphics;
 
 /**
- * UI Panel = single container with local coordinate space.
+ * Fixed UI container.
  */
 public class Panel {
 
-    public final int x;
-    public final int y;
-    public final int w;
-    public final int h;
+    public final int x, y, w, h;
 
     public Panel(int x, int y, int w, int h) {
         this.x = x;
@@ -19,7 +16,7 @@ public class Panel {
         this.h = h;
     }
 
-    public void drawBase(GuiGraphics g) {
+    public void draw(GuiGraphics g) {
         g.fill(x, y, x + w, y + h, 0xFF141414);
         g.fill(x, y, x + w, y + 18, 0xFF1C1C1C);
     }
