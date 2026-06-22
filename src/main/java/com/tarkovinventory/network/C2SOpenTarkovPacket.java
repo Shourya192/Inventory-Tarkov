02620f58f1d2abbac17b0f6eb9689f4c17219cb4
Shourya192 <90411872+Shourya192@users.noreply.github.com>
2026-06-22 16:40:32 +0530

    Update C2SOpenTarkovPacket.java

--- src/main/java/com/tarkovinventory/network/C2SOpenTarkovPacket.java
+++ src/main/java/com/tarkovinventory/network/C2SOpenTarkovPacket.java
@@ -3,7 +3,14 @@ package com.tarkovinventory.network;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.network.NetworkEvent;
-import net.minecraftforge.network.PacketDistributor;
+import com.tarkovinventory.container.TarkovInventoryMenu;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraftforge.network.NetworkHooks;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Supplier;
 
@@ -22,10 +29,17 @@ public class C2SOpenTarkovPacket {
             ServerPlayer player = ctx.get().getSender();
             if (player == null) return;
 
-            ModNetwork.CHANNEL.send(
-                    PacketDistributor.PLAYER.with(() -> player),
-                    new S2COpenTarkovPacket()
-            );
+            NetworkHooks.openScreen(player, new MenuProvider() {
+                @Override
+                public @NotNull Component getDisplayName() {
+                    return Component.literal("Tarkov Inventory");
+                }
+
+                @Override
+                public @NotNull AbstractContainerMenu createMenu(int windowId, @NotNull Inventory inv, @NotNull Player p) {
+                    return new TarkovInventoryMenu(windowId, inv, 0);
+                }
+            }, buf -> buf.writeInt(0));
         });
 
         ctx.get().setPacketHandled(true);
